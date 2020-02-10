cbfca34f6fc87bc3b02b0b728dd2fed1ee7f9ff7
Abed Islam <aislam360@gmail.com>
2020-02-10 18:01:06 -0500

    Added list, need to make the container

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React from 'react';
 import './App.css';
 import Character from './components/Character.js'
 function App() {
+
+  let list = [{
+    name:'Cinder', 
+    power: 'fire', 
+    color: 'orange', 
+    age: 28, 
+    moves: ["Trailblazer", "Inferno", "Fire Flash"]
+  },
+  {
+    name:'Aganos', 
+    power: 'rock', 
+    color: '#009999', 
+    age: 900000, 
+    moves:["Payload Assault", "Pulverize", "Ruin"]
+  }, 
+  {
+    name:'Jago', 
+    power: 'Tiger Spirit', 
+    color: '#336699', 
+    age: 25, 
+    moves:["Edokuken", "Tiger Fury", "Wind Kick"]
+  }];
+
+  let charStats = list.map((it, idx)=>
+    <Character name={it.name} power={it.power} age={it.age} moves={it.moves}/>
+
+  );
+
+
   return (
     <div className="App">
-
+      {charStats}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,31 +1,17 @@
 import React from "react";
 
-function Character(){
+function Character(props){
 
-	let charStats = [{
-		name:'Cinder', 
-		power: 'fire', 
-		color: 'orange', 
-		age: 28, 
-		moves: ["Trailblazer", "Inferno", "Fire Flash"]
-	},
-	{
-		name:'Aganos', 
-		power: 'rock', 
-		color: '#009999', 
-		age: 900000, 
-		moves:["Payload Assault", "Pulverize", "Ruin"]
-	}, 
-	{
-		name:'Jago', 
-		power: 'Tiger Spirit', 
-		color: '#336699', 
-		age: 25, 
-		moves:["Edokuken", "Tiger Fury", "Wind Kick"]
-	}]
+
 
 	return(
-
+		<div>
+			<h3>Name: {props.name}</h3>
+			<h3>Superpower: {props.power}</h3>
+			<h3>Age: {props.age}</h3>
+			<h3>Special Movies:</h3>
+			<h3>{props.moves}</h3>
+		</div>
 
 	)
 }
