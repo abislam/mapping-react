c910bf6f3c33a3b2560a728d436156b01ed60387
Abed Islam <aislam360@gmail.com>
2020-02-10 17:47:47 -0500

    Initial commit

---
+++ src/components/Character.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Character(){
+
+	let charStats = [{
+		name:'Cinder', 
+		power: 'fire', 
+		color: 'orange', 
+		age: 28, 
+		moves: ["Trailblazer", "Inferno", "Fire Flash"]
+	},
+	{
+		name:'Aganos', 
+		power: 'rock', 
+		color: '#009999', 
+		age: 900000, 
+		moves:["Payload Assault", "Pulverize", "Ruin"]
+	}, 
+	{
+		name:'Jago', 
+		power: 'Tiger Spirit', 
+		color: '#336699', 
+		age: 25, 
+		moves:["Edokuken", "Tiger Fury", "Wind Kick"]
+	}]
+
+	return(
+
+
+	)
+}
+
+
+export default Character;
