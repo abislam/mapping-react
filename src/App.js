1c38b9dff67fd69effae263c5b3997e6d2a6f798
Abed Islam <aislam360@gmail.com>
2020-02-10 20:57:33 -0500

    Need to create the onClick functionality

--- src/App.js
+++ src/App.js
@@ -3,37 +3,67 @@ import './App.css';
 import Character from './components/Character.js'
 function App() {
 
-  let list = [{
-    name:'Cinder', 
-    power: 'fire', 
-    color: 'orange', 
-    age: 28, 
-    moves: ["Trailblazer", "Inferno", "Fire Flash"]
-  },
-  {
-    name:'Aganos', 
-    power: 'rock', 
-    color: '#009999', 
-    age: 900000, 
-    moves:["Payload Assault", "Pulverize", "Ruin"]
-  }, 
-  {
-    name:'Jago', 
-    power: 'Tiger Spirit', 
-    color: '#336699', 
-    age: 25, 
-    moves:["Edokuken", "Tiger Fury", "Wind Kick"]
-  }];
+  let list = [
+    {
+      name:'Cinder', 
+      power: 'fire', 
+      color: 'orange', 
+      age: 28, 
+      moves: ["Trailblazer", "Inferno", "Fire Flash"]
+    },
+    {
+      name:'Aganos', 
+      power: 'rock', 
+      color: '#009999', 
+      age: 900000, 
+      moves:["Payload Assault", "Pulverize", "Ruin"]
+    }, 
+    {
+      name:'Jago', 
+      power: 'Tiger Spirit', 
+      color: '#336699', 
+      age: 25, 
+      moves:["Edokuken", "Tiger Fury", "Wind Kick"]
+    }
+  ];
+
+  let myList = [
+    {
+      name: 'Goku',
+      power: 'Super Saiyan Blue',
+      color: 'blue',
+      age: 36,
+      moves: ["Spirit Bomb", "Kaioken", "Kamehameha"]
+    },
+    {
+      name: 'Gohan',
+      power: 'Ultimate Gohan',
+      color: 'Purple',
+      age: 26,
+      moves: ["Father Son Kamehameha", "Masenko", "Kamehameha"]
+    },
+    {
+      name: 'Krillin',
+      power: 'Bald Head',
+      color: 'Red',
+      age: 34,
+      moves: ["Destructo Disc", "Solar Flare", "Kamehameha"]
+    }
+  ];
+
 
   let charStats = list.map((it, idx)=>
-    <Character name={it.name} power={it.power} age={it.age} moves={it.moves}/>
-
+    <Character name={it.name} power={it.power} age={it.age} moves={it.moves} color={it.color} key={idx}/>
   );
 
+  let myCharStats = myList.map((x, idx)=>
+    <Character name={x.name} power={x.power} age={x.age} moves={x.moves} color={x.color} key={idx}/>
+  )
 
   return (
     <div className="App">
       {charStats}
+      {myCharStats}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "../character.css";
 
 function Character(props){
 
-
-
 	return(
-		<div>
+
+		<div className="character" style={{backgroundColor: props.color}}>	
 			<h3>Name: {props.name}</h3>
 			<h3>Superpower: {props.power}</h3>
 			<h3>Age: {props.age}</h3>
-			<h3>Special Movies:</h3>
+			<h3>Special Moves:</h3>
 			<h3>{props.moves + "\n"}</h3>
 		</div>
+
 
 	)
 }
